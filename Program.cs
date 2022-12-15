6909304a3efeba3b38b3391f3e794029c12b789b
anushkachouhan029 <anushkachouhan029>
2022-12-15 23:05:52 +0530

    added validate name

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Regex_User_Registration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Regular Expression");
+            Console.WriteLine("Enter your first name");
+            string FirstName = Console.ReadLine(); // Creating user input for first name
+            Validation.FirstNameValidation(FirstName);//Calling method to validate firstname
+        }
+    }
+}
---
+++ UR1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_User_Registration
+{
+    internal class Validation
+    {
+
+        public static void FirstNameValidation(string FirstName)
+        {
+            string pattern = "^[A-Z][a-z]{2,}$"; // Creating REGEX pattern
+            if (Regex.IsMatch(FirstName, pattern)) //For checking regex are valid or invalid
+            {
+                Console.WriteLine("The First name is : " + FirstName);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Name : " + FirstName);
+            }
+        }
+    }
+}
