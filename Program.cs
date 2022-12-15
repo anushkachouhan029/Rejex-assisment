6d6e569572acd07ce4044ca1d623ca8c56e821df
anushkachouhan029 <anushkachouhan029>
2022-12-16 00:04:30 +0530

    added passwordrule1

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace UserRegistrationPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the User Registration Program\n========================================");
+            DataValidation validating = new DataValidation();
+            bool end = true;
+            Console.WriteLine("1. Validate First Name\n2. Validate Last Name\n3. Validate Email\n4. " +
+                "Validate Phone Number\n5. Validate Password With Rule 1\n6. End The Program");
+            while (end)
+            {
+                Console.Write("Choose an option to execute : ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        validating.ValidateFirstName();
+                        break;
+                    case 2:
+                        validating.ValidateLastName();
+                        break;
+                    case 3:
+                        validating.ValidateEmail("abc.xyz@bl.co.in");
+                        validating.ValidateEmail("abc111@abc.com");
+                        validating.ValidateEmail("abc@bl.co");
+                        validating.ValidateEmail("abc1232.com");
+                        break;
+                    case 4:
+                        validating.ValidatePhoneNumber();
+                        break;
+                    case 5:
+                        PasswordRules validate = new PasswordRules();
+                        validate.ValidatePassword();
+                        break;
+                    case 6:
+                        end = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please choose correct option");
+                        break;
+                }
+            }
+        }
+    }
+}
