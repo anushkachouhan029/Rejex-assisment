8365fc8be06fed7e498aa81233faa93bde75f3fb
anushkachouhan029 <anushkachouhan029>
2022-12-16 00:12:27 +0530

    added password rule 2

---
+++ DataValidation.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationPrograms
+{
+    public class DataValidation
+    {
+        public Regex First_Name_Regex = new Regex(@"^[A-Z]{1}[A-Za-z]{2,}?$");
+        public void ValidateFirstName()
+        {
+            Console.Write("Enter First Name : ");
+            string firstName = Console.ReadLine();
+
+            if (First_Name_Regex.IsMatch(firstName))
+            {
+                Console.WriteLine("The First Name Is Valid ");
+            }
+            else
+            {
+                Console.WriteLine("The First Name Is Not Valid");
+            }
+        }
+        public Regex Last_Name_Regex = new Regex(@"^[A-Z]{1}[A-Za-z]{2,}?$");
+        public void ValidateLastName()
+        {
+            Console.Write("Enter Last Name : ");
+            string lastName = Console.ReadLine();
+
+            if (Last_Name_Regex.IsMatch(lastName))
+            {
+                Console.WriteLine("The Last Name Is Valid ");
+            }
+            else
+            {
+                Console.WriteLine("The Last Name Is Not Valid");
+            }
+        }
+        public Regex Email_Regex = new Regex("^[A-Za-z]{3,}([.][A-Za-z]{3,})?[@][A-Za-z]{2,}[.][A-Za-z]{2,}([.][A-Za-z]{2})?$");
+        public void ValidateEmail(string email)
+        {
+            if (Email_Regex.IsMatch(email))
+            {
+                Console.WriteLine("Email Is Valid ");
+            }
+            else
+            {
+                Console.WriteLine("Email Is Not Valid ");
+            }
+        }
+        public Regex PhoneNumber_Regex = new Regex("[1-9][0-9][ ]?[6-9][0-9]{9}$");
+        public void ValidatePhoneNumber()
+        {
+            Console.Write("Enter Phone Number : ");
+            string phoneNumber = Console.ReadLine();
+
+            if (PhoneNumber_Regex.IsMatch(phoneNumber))
+            {
+                Console.WriteLine("Phone Number Is Valid ");
+            }
+            else
+            {
+                Console.WriteLine("Phone Number Is Not Valid ");
+            }
+
+
+        }
+    }
+}
---
+++ PasswordRules.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationPrograms
+{
+    public class PasswordRules
+    {
+        public Regex Password_Regex = new Regex("^[A-Za-z0-9]{8,16}$");
+        public void ValidatePassword()
+        {
+            Console.Write("Enter the password : ");
+            string password = Console.ReadLine();
+
+            if (Password_Regex.IsMatch(password))
+            {
+                Console.WriteLine("The password is valid ");
+            }
+            else
+            {
+                Console.WriteLine("Invalid passord");
+            }
+        }
+        public Regex Password_Regex2 = new Regex("^[A-Z]{1,8}[a-z0-9]{7,}$");
+        public void ValidatePassword2()
+        {
+            Console.Write("Enter the password : ");
+            string password2 = Console.ReadLine();
+
+            if (Password_Regex2.IsMatch(password2))
+            {
+                Console.WriteLine("The password is valid ");
+            }
+            else
+            {
+                Console.WriteLine("Invalid passord");
+            }
+        }
+
+    }
+}
