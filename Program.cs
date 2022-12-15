663a3876dfe5c45d1f5ba7a2094a562f22d2c4a8
anushkachouhan029 <anushkachouhan029>
2022-12-15 23:34:03 +0530

    added email validation

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Regex_User_Registration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcome to Regular Expression");
+            //Console.WriteLine("Enter your first name");
+            //string FirstName = Console.ReadLine(); // Creating user input for first name
+            //Validation.FirstNameValidation(FirstName);//Calling method to validate firstname
+
+
+            //Console.WriteLine("Enter your Last name");
+            //string LName = Console.ReadLine(); //  input for last name
+            //Validation.LastNameValidation(LName);
+
+            Console.WriteLine("\n\nEnter your email name : ");
+            string email = Console.ReadLine();
+            bool checkEmail = UR3_Email_Validation.EmailValidation(email); //Calling method to check email 
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
+Footer
+© 2022 GitHub, Inc.
